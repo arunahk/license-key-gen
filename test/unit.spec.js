afaa9d617e39aea925b26a3017351609f8ed0419
Aruna Lakmal <arunalakmal@gmail.com>
2018-04-17 17:50:25 +1200

    add travis config

--- test/unit.spec.js
+++ test/unit.spec.js
@@ -1,6 +1,6 @@
 'use strict'
 
-/* Dev libs*/
+/* Dev libs */
 const chai = require('chai')
 const expect = chai.expect
 
@@ -19,7 +19,7 @@ describe('createLicense', () =>
       country: 'New Zealand',
       zip: '8014'
     }
-    let userData = {info: userInfo, prodCode: "TEST01", appVersion: "0.0.1."}
+    let userData = {info: userInfo, prodCode: 'TEST01', appVersion: '0.0.1.'}
     let license = LicenseGen.createLicense(userData)
 
     expect(license).to.be.an('object')
@@ -42,7 +42,7 @@ describe('validateLicense', () =>
       country: 'New Zealand',
       zip: '8014'
     }
-    let userData = {info: userInfo, prodCode: "TEST01", appVersion: "0.0.1."}
+    let userData = {info: userInfo, prodCode: 'TEST01', appVersion: '0.0.1.'}
     let validity = LicenseGen.validateLicense(userData, 'Y35V5-41651-4VB72-D2C86-27748-A5B')
 
     expect(validity).to.be.an('object')
