55e6410f45bd4a97cf3cefc279c720e5194cc690
Aruna Lakmal <contact@webisto.tech>
2020-02-07 10:58:48 +1300

    added WebStorm project files and update tests

--- test/unit.spec.js
+++ test/unit.spec.js
@@ -12,15 +12,15 @@ describe('createLicense', () =>
   console.time('createLicense')
   it('return proper license', () =>
   {
-    let userInfo = {
+    const userInfo = {
       company: 'Webisto Tech',
       department: 'IT',
       city: 'Christchurch',
       country: 'New Zealand',
       zip: '8014'
     }
-    let userData = {info: userInfo, prodCode: 'TEST01', appVersion: '0.0.1.'}
-    let license = LicenseGen.createLicense(userData)
+    const userData = { info: userInfo, prodCode: 'TEST01', appVersion: '0.0.1.' }
+    const license = LicenseGen.createLicense(userData)
 
     expect(license).to.be.an('object')
     expect(license).to.have.property('message', 'ok')
@@ -35,15 +35,15 @@ describe('validateLicense', () =>
   console.time('validateLicense')
   it('validate license', () =>
   {
-    let userInfo = {
+    const userInfo = {
       company: 'Webisto Tech',
       department: 'IT',
       city: 'Christchurch',
       country: 'New Zealand',
       zip: '8014'
     }
-    let userData = {info: userInfo, prodCode: 'TEST01', appVersion: '0.0.1.'}
-    let validity = LicenseGen.validateLicense(userData, 'Y35V5-41651-4VB72-D2C86-27748-A5B')
+    const userData = { info: userInfo, prodCode: 'TEST01', appVersion: '0.0.1.' }
+    const validity = LicenseGen.validateLicense(userData, 'Y35V5-41651-4VB72-D2C86-27748-A5B')
 
     expect(validity).to.be.an('object')
     expect(validity).to.have.property('message', 'ok')
